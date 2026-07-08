965356ecf938133b7097ddae608701e81f6fe2b4
mohammadhasansojib <sajibhasan800080@gmail.com>
2026-07-08 23:13:11 +0600

    chore: add some environment variables and import

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,11 @@ dotenv.config()
 
 export const config = {
     database_url: process.env.DATABASE_URL,
+    port: Number(process.env.PORT),
+    cookie_parser_secret: process.env.COOKIE_PARSER_SECRET,
+    jwt_secret: process.env.JWT_SECRET,
+    jwt_expires_in: Number(process.env.JWT_EXPIRES_IN),
+    access_token_cookie_max_age: Number(process.env.ACCESS_TOKEN_COOKIE_MAX_AGE),
 }
 
 export default config;
